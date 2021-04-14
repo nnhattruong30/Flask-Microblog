99e15f3c2c90496908a51b450f01c58715f45207
Nguyen N.Truong <nnhattruong30@gmail.com>
2021-04-14 21:39:01 +0700

    Feature: add logger

--- app/__init__.py
+++ app/__init__.py
@@ -34,3 +34,15 @@ if not app.debug:
             secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+    # Logger
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('log/microblog.log', maxBytes=10240,
+                                        backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
